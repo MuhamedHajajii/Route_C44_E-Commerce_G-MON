c5a6b4cdc9e503ed63b77462149a4592530fba1f
haji <muhamedhajajii@outlook.com>
2025-02-25 03:06:35 +0200

    dist

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,7 +1,6 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  { path: 'home', renderMode: RenderMode.Client },
   {
     path: '**',
     renderMode: RenderMode.Server,
