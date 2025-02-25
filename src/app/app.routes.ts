cab37b1a2d7f38243b27689e5810bf8d6fa2d847
haji <muhamedhajajii@outlook.com>
2025-02-25 02:28:57 +0200

    dist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,13 @@ import { SignUpComponent } from './auth/page/sign-up/sign-up.component';
 
 import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
-import { BrandsComponent } from './pages/main/brands/brands.component';
-import { CartComponent } from './pages/main/cart/cart.component';
-import { CategoriesComponent } from './pages/main/categories/categories.component';
-import { DetailsComponent } from './pages/main/details/details.component';
-import { HomeComponent } from './pages/main/home/home.component';
 import { NotfoundComponent } from './pages/main/notfound/notfound.component';
-import { ProductsComponent } from './pages/main/products/products.component';
+// import { HomeComponent } from './pages/main/home/home.component';
+// import { ProductsComponent } from './pages/main/products/products.component';
+// import { CartComponent } from './pages/main/cart/cart.component';
+// import { CategoriesComponent } from './pages/main/categories/categories.component';
+// import { BrandsComponent } from './pages/main/brands/brands.component';
+// import { DetailsComponent } from './pages/main/details/details.component';
 import { SignInComponent } from './auth/page/sign-in/sign-in.component';
 import { authGuard } from './core/guards/auth.guard';
 import { logedUserGuard } from './core/guards/loged-user.guard';
@@ -32,12 +32,48 @@ export const routes: Routes = [
     canActivate: [authGuard],
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'Products', component: ProductsComponent },
-      { path: 'cart', component: CartComponent },
-      { path: 'categories', component: CategoriesComponent },
-      { path: 'brands', component: BrandsComponent },
-      { path: 'details/:id', component: DetailsComponent },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/main/home/home.component').then(
+            (e) => e.HomeComponent
+          ),
+      },
+      {
+        path: 'Products',
+        loadComponent: () =>
+          import('./pages/main/products/products.component').then(
+            (e) => e.ProductsComponent
+          ),
+      },
+      {
+        path: 'cart',
+        loadComponent: () =>
+          import('./pages/main/cart/cart.component').then(
+            (e) => e.CartComponent
+          ),
+      },
+      {
+        path: 'categories',
+        loadComponent: () =>
+          import('./pages/main/categories/categories.component').then(
+            (e) => e.CategoriesComponent
+          ),
+      },
+      {
+        path: 'brands',
+        loadComponent: () =>
+          import('./pages/main/brands/brands.component').then(
+            (e) => e.BrandsComponent
+          ),
+      },
+      {
+        path: 'details/:id',
+        loadComponent: () =>
+          import('./pages/main/details/details.component').then(
+            (e) => e.DetailsComponent
+          ),
+      },
     ],
   },
   { path: '**', component: NotfoundComponent },
